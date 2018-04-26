ab063dc2ad5cdcdcb427a1db9b034211b5be392f
Michael Arnold <vehk@protonmail.com>
2018-04-26 02:52:06 -0700

    Updated to PEP8

--- SpookyBot/spooky_bot.py
+++ SpookyBot/spooky_bot.py
@@ -1,4 +1,4 @@
-# Spooky Bot Version 0.5.20
+# Spooky Bot Version 0.5.21
 # Written in Python 3.6.4
 import discord
 from discord.ext import commands
@@ -13,27 +13,34 @@ import shorten_url
 import f_commands
 import imgur_upload
 
-bot = commands.Bot(command_prefix='!', description=\
+bot = commands.Bot(command_prefix='!', description=
                    "Spooky Bot - So good it's spooky!", pm_help=True)
 client = discord.Client(max_messages=100)
+
 
 @bot.event
 async def on_ready():
     print('Logged in as: ')
     print(bot.user.name)
-    await bot.change_presence(game=discord.Game(name='spooky music'),\
+    await bot.change_presence(game=discord.Game(name='spooky music'),
                               status=discord.Status.online)
- 
+
+
 @bot.command()
 async def halloween():
+    # Returns time until Halloween (PST) for the user.
     await bot.say(is_it_halloween())
+
 
 @bot.command()
 async def batfact():
+    # Returns random bat fact for the user.
     await bot.say(f_facts('bat'))
+
 
 @bot.command()
 async def commands(query : str=None):
+    # Returns a list of commands or information about specific commands.
     if not query:
         await bot.say(f_commands.help('commands'))
     try:
@@ -42,8 +49,10 @@ async def commands(query : str=None):
         print(f'User generated the error {e} after entering {query} in help.')
         await bot.say('Sorry, something went wrong. :(')
 
+
 @bot.command()
 async def roll(num : str=None):
+    # Rolls a random number for the user.
     if not num or not num[0].isdigit() and not num[0].startswith('-'):
         await bot.say(f_roll(100))
         return
@@ -53,8 +62,10 @@ async def roll(num : str=None):
         print(f'User generated the error {e} after entering: "{num}')
         await bot.say(f'Please use the correct format: !roll number')
 
+
 @bot.command()
 async def flip(num : str=None):
+    # Flips coins to determine heads or tails for the user.
     if not num or not num[0].isdigit() and not num[0].startswith('-'):
         await bot.say(f_flip(1))
         return
@@ -64,12 +75,17 @@ async def flip(num : str=None):
         print(f'User generated the error {e} after entering: "{num}"')
         await bot.say('Please use the correct format: !flip number')
 
+
 @bot.command()
 async def imgur(*args):
+    # Scrapes a given sub-reddit for pictures and uploads them into
+    # an Imgur album.
     await bot.say(imgur_upload.main(args))
+
 
 @bot.command()
 async def answer(*args):
+    # Feeds the user input into Wolfram Alpha for an answer.
     # Note to self, should probably replace *args here with question : str=None
     if not args:
         await bot.say('Please ask a question Wolfram Alpha can answer!')
@@ -80,8 +96,10 @@ async def answer(*args):
         print(f'User generated the error {e} after entering: "{args}"')
         await bot.say("I couldn't find an answer for that, sorry. :(")
 
+
 @bot.command()
 async def shorten(*args):
+    # Reduces a URL length for the user.
     if not args:
         await bot.say('Please make sure to use the correct format: !shorten link')
     try:
@@ -92,14 +110,16 @@ async def shorten(*args):
         print(f'User generated the error {e} after entering: "{args}"')
         await bot.say("Something went wrong, sorry! :(")
 
+
 @bot.command()
 async def ud(*args):
+    # Defines a word using UrbanDictionary.com
     if not args:
         await bot.say('Please make sure you use the correct format: !ud word')
     try:
         definition = urban_define.ud_define(' '.join(args))
-        embed = discord.Embed(title="Here is your Urban "\
-                "Dictionary definition!", color=0x00ff00)
+        embed = discord.Embed(title="Here is your Urban "
+                                    "Dictionary definition!", color=0x00ff00)
         embed.add_field(name="Word", value=definition[0], inline=True)
         embed.add_field(name="Meaning", value=definition[1], inline=True)
         embed.add_field(name="Example", value=definition[2], inline=True)
@@ -110,8 +130,10 @@ async def ud(*args):
         print(f'User generated the error {e} after entering: "{args}"')
         await bot.say("Sorry, your word or phrase wasn't found. :(")
 
+
 @bot.command(pass_context=True)
 async def info(user: discord.Member):
+    # Provides information about a user in the channel.
     embed = discord.Embed(title="{}'s info".format(user.name), description=\
                           "Here's what I could find.", color=0x00ff00)
     embed.add_field(name="Name", value=user.name, inline=True)
