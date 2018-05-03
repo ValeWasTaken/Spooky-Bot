288758d0c3a9ed867a11eadba6eff1dc38db1511
Michael Arnold <vehk@protonmail.com>
2018-05-02 23:22:43 -0700

    Organized functions by alphabetical order

--- SpookyBot/spooky_bot.py
+++ SpookyBot/spooky_bot.py
@@ -1,4 +1,4 @@
-# Spooky Bot Version 0.5.311
+# Spooky Bot Version 0.5.33
 # Written in Python 3.6.4
 import discord
 from discord.ext import commands
@@ -29,39 +29,22 @@ async def on_ready():
 
 
 @bot.command()
-async def halloween():
-    # Returns time until Halloween (PST) for the user.
-    await bot.say(is_it_halloween())
+async def answer(*args):
+    # Feeds the user input into Wolfram Alpha for an answer.
+    if not args:
+        await bot.say('Please ask a question Wolfram Alpha can answer!')
+    try:
+        question = ' '.join(args)
+        await bot.say(wolfram_alpha.solve(question))
+    except Exception as e:
+        print(f'User generated the error {e} after entering: "{args}"')
+        await bot.say("I couldn't find an answer for that, sorry. :(")
 
 
 @bot.command()
 async def batfact():
     # Returns random bat fact for the user.
     await bot.say(f_facts('bat'))
-
-
-@bot.command()
-async def commands(query : str=None):
-    # Returns a list of commands or information about specific commands.
-    if not query:
-        await bot.say(f_commands.h('commands'))
-    try:
-        await bot.say(f_commands.h(query))
-    except Exception as e:
-        print(f'User generated the error {e} after entering {query} in commands.')
-
-
-@bot.command()
-async def roll(num : str=None):
-    # Rolls a random number for the user.
-    if not num or not num[0].isdigit() and not num[0].startswith('-'):
-        await bot.say(f_roll(100))
-        return
-    try:
-        await bot.say(f_roll(int(num)))
-    except Exception as e:
-        print(f'User generated the error {e} after entering: "{num}')
-        await bot.say(f'Please use the correct format: !roll number')
 
 
 @bot.command()
@@ -86,16 +69,18 @@ async def check(symbol: str = None):
 
 
 @bot.command()
-async def reverse(*args):
-    if not args:
-        await bot.say(f_flip_text('What do you want me to flip?'))
-    else:
-        message = ' '.join(args)
-        await bot.say(f_flip_text(message))
+async def commands(query: str=None):
+    # Returns a list of commands or information about specific commands.
+    if not query:
+        await bot.say(f_commands.h('commands'))
+    try:
+        await bot.say(f_commands.h(query))
+    except Exception as e:
+        print(f'User generated the error {e} after entering {query} in commands.')
 
 
 @bot.command()
-async def flip(num : str=None):
+async def flip(num: str=None):
     # Flips coins to determine heads or tails for the user.
     if not num or not num[0].isdigit() and not num[0].startswith('-'):
         await bot.say(f_flip(1))
@@ -108,24 +93,52 @@ async def flip(num : str=None):
 
 
 @bot.command()
+async def halloween():
+    # Returns time until Halloween (PST) for the user.
+    await bot.say(is_it_halloween())
+
+
+@bot.command()
 async def imgur(*args):
     # Scrapes a given sub-reddit for pictures and uploads them into
     # an Imgur album.
     await bot.say(imgur_upload.main(args))
+    
+
+@bot.command(pass_context=True)
+async def info(user: discord.Member):
+    # Provides information about a user in the channel.
+    embed = discord.Embed(title="{}'s info".format(user.name), description=\
+                          "Here's what I could find.", color=0x00ff00)
+    embed.add_field(name="Name", value=user.name, inline=True)
+    embed.add_field(name="ID", value=user.id, inline=True)
+    embed.add_field(name="Status", value=user.status, inline=True)
+    embed.add_field(name="Highest role", value=user.top_role)
+    embed.add_field(name="Joined", value=user.joined_at)
+    embed.set_thumbnail(url=user.avatar_url)
+    await bot.say(embed=embed)
+    
+
+@bot.command()
+async def reverse(*args):
+    if not args:
+        await bot.say(f_flip_text('What do you want me to flip?'))
+    else:
+        message = ' '.join(args)
+        await bot.say(f_flip_text(message))
 
 
 @bot.command()
-async def answer(*args):
-    # Feeds the user input into Wolfram Alpha for an answer.
-    # Note to self, should probably replace *args here with question : str=None
-    if not args:
-        await bot.say('Please ask a question Wolfram Alpha can answer!')
+async def roll(num: str=None):
+    # Rolls a random number for the user.
+    if not num or not num[0].isdigit() and not num[0].startswith('-'):
+        await bot.say(f_roll(100))
+        return
     try:
-        question = ' '.join(args)
-        await bot.say(wolfram_alpha.solve(question))
+        await bot.say(f_roll(int(num)))
     except Exception as e:
-        print(f'User generated the error {e} after entering: "{args}"')
-        await bot.say("I couldn't find an answer for that, sorry. :(")
+        print(f'User generated the error {e} after entering: "{num}')
+        await bot.say(f'Please use the correct format: !roll number')
 
 
 @bot.command()
@@ -160,20 +173,6 @@ async def ud(*args):
     except Exception as e:
         print(f'User generated the error {e} after entering: "{args}"')
         await bot.say("Sorry, your word or phrase wasn't found. :(")
-
-
-@bot.command(pass_context=True)
-async def info(user: discord.Member):
-    # Provides information about a user in the channel.
-    embed = discord.Embed(title="{}'s info".format(user.name), description=\
-                          "Here's what I could find.", color=0x00ff00)
-    embed.add_field(name="Name", value=user.name, inline=True)
-    embed.add_field(name="ID", value=user.id, inline=True)
-    embed.add_field(name="Status", value=user.status, inline=True)
-    embed.add_field(name="Highest role", value=user.top_role)
-    embed.add_field(name="Joined", value=user.joined_at)
-    embed.set_thumbnail(url=user.avatar_url)
-    await bot.say(embed=embed)
     
     
 bot.run("INSERT BOT'S SECRET TOKEN HERE")
