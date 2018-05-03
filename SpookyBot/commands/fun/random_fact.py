f458c72d721af2947039c3325dd63e297a97c350
Mundayne <mundanebackflip@gmail.com>
2018-05-03 21:26:36 +1200

    forgot this

--- SpookyBot/commands/fun/random_fact.py
+++ SpookyBot/commands/fun/random_fact.py
@@ -1,10 +1,10 @@
 from random import shuffle  # Used to randomly mix list of facts.
-
+import os
 
 def random_fact(subject):
     # NOT FOR USER INPUT.
     # Allows <subject>facts.txt to be called for reusability.
-    with open(f'../../resources/facts/{subject}.txt', 'r') as f:
+    with open(os.path.realpath(f'../../resources/facts/{subject}.txt'), 'r') as f:
         facts = f.readlines()
         shuffle(facts)
         fact = facts[0].replace('\n', '')
